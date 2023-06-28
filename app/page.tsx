2902c943c9fdc4a3d0701cd69ac6dd48b44e2f9b
Jim Jin <jim_king_2000@163.com>
2023-06-28 13:43:08 +0800

    init project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Grommet, Box, Text } from 'grommet';
+
+export default function Page() {
+  return (
+    <Grommet>
+      <Box>
+        <Text>Grommet</Text>
+      </Box>
+    </Grommet>
+  );
+}
