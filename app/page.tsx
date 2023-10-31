434b541a20db93a2fcd8b59fb264d7f33758c01c
Jim Jin <jim_king_2000@163.com>
2023-10-31 15:30:55 +0800

    refactory

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,17 @@
 'use client';
 
-import { useState } from 'react';
-
 export default function Page() {
-  const [fs, setFs] = useState(false);
   function toggleFullScreen() {
-    if (fs) {
+    if (document.fullscreenElement) {
       document.exitFullscreen();
       screen.orientation.unlock();
-      setFs(false);
     } else {
       document.documentElement.requestFullscreen();
       screen.orientation.lock('landscape');
-      setFs(true);
     }
   }
 
   return (
-      <button onClick={() => toggleFullScreen()}>{fs ? 'unlock' : 'lock'}</button>
+    <button onClick={() => toggleFullScreen()}>lock/unlock</button>
   );
 }
